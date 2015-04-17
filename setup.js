303c080a74953b9c57c5568b612af3d1125789ba
Leif Dalan <leifdalan@gmail.com>
2015-04-17 09:43:34 -0700

    Removed config require for setup, since setup actually makes the config.

--- setup.js
+++ setup.js
@@ -1,10 +1,9 @@
 'use scrict';
-var BABEL_STAGE = require('./config').BABEL_STAGE;
 
 // Babel entry point
 require('babel/register')({
   ignore: /node_modules\/(?!react-router)/,
-  stage: BABEL_STAGE
+  stage: 0
 });
 
 // Server code, including ES6/7 syntax
